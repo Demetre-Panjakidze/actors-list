2b0b085259827961dcef69d9a1c6f4c6f37b9c93
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-01-31 23:56:04 +0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { InfoComponent } from './components/info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
