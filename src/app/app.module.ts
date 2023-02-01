41cacdefb21e4bf019cef0dc0161f71745650f40
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-02-01 04:11:40 +0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
 import { InfoComponent } from './components/info/info.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, SearchComponent, InfoComponent, NotFoundComponent],
+  declarations: [AppComponent, SearchComponent, InfoComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
