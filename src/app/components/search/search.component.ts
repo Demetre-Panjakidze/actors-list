d06ac642ffeb94b1a806adcb9ee2651c30fb14d3
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-02-02 12:04:38 +0400

    initial commit

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -6,25 +6,7 @@ import json from '../../../assets/data/customer-list.json';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
   actorsList = json;
   selectedOption = '';
-  btnDisabled: boolean = false;
-  searchDisplayed: boolean = true;
-
-  checkValue = () => {
-    if (!this.selectedOption) {
-      this.btnDisabled = true;
-    } else {
-      this.btnDisabled = false;
-    }
-  };
-
-  selectedOptionChange(event: any) {
-    this.checkValue();
-  }
-
-  ngOnInit() {
-    this.checkValue();
-  }
 }
